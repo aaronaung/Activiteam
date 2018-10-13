aa53fd8e8e807920955b597d7f354bb9b67e6dc4
Dennis Dang <dangggdennis@gmail.com>
2018-10-13 16:40:04 -0700

    Style login screen

--- client/prettier.config.js
+++ client/prettier.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    'prettier.printWidth': 100,
-    'prettier.tabWidth': 4,
-    'prettier.useTabs': false,
-    'prettier.semi': true,
-    'prettier.singleQuote': true,
-    'prettier.trailingComma': 'none',
-    'prettier.bracketSpacing': true,
-    'prettier.jsxBracketSameLine': false
+  "prettier.printWidth": 100,
+  "prettier.tabWidth": 4,
+  "prettier.useTabs": false,
+  "prettier.semi": true,
+  "prettier.singleQuote": true,
+  "prettier.trailingComma": true,
+  "prettier.bracketSpacing": true,
+  "prettier.jsxBracketSameLine": false
 };
--- client/src/screens/LoginScreen.tsx
+++ client/src/screens/LoginScreen.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import colors from "../styles/colors";
+import headers from "../styles/headers";
+import Login from "../components/Login";
+
 export default class LoginScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Login Screen!</Text>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>ACTIVITEAM</Text>
+        </View>
+        <View style={styles.loginContainer}>
+          <Login />
+        </View>
       </View>
     );
   }
@@ -14,8 +23,21 @@ export default class LoginScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: colors.greyDark,
+    justifyContent: "space-around"
+  },
+  titleContainer: {
+    flex: 2,
+    alignSelf: 'center',
     justifyContent: "center"
+  },
+  title: {
+    ...headers.one,
+    color: colors.white,
+    letterSpacing: 8,
+    fontWeight: "600"
+  },
+  loginContainer: {
+    flex: 3,
   }
 });
