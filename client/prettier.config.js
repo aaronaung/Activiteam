70f4990088ff71d04ebc5744ac01ba7cd6fdbced
Dennis Dang <dangggdennis@gmail.com>
2018-10-13 10:34:46 -0700

    Adds typescript to react native

---
+++ client/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'prettier.printWidth': 100,
+    'prettier.tabWidth': 4,
+    'prettier.useTabs': false,
+    'prettier.semi': true,
+    'prettier.singleQuote': true,
+    'prettier.trailingComma': 'none',
+    'prettier.bracketSpacing': true,
+    'prettier.jsxBracketSameLine': false
+};
